7797731f3c796d0be417ece0c28e62b64a898ec4
Luis Angel <luis.pena.15s@utzmg.edu.mx>
2019-04-23 12:43:38 -0500

    Get object with te comps divided by exist in the original code

--- mergeEsp.js
+++ mergeEsp.js
@@ -11,69 +11,31 @@ const cnctRootEsp = R.map(file => rootData + file)
 
 const omitFls = R.without(cnctRootEsp(['MenuPrincipal_DLG_MAVI.esp']))
 
-const origFls = pathFile => {
-    return {
-        ['esp']: pathFile,
-        ['orig']: 'c:\\Users\\lapena\\Documents\\Luis Angel\\Sección Mavi\\'
-        + 'Intelisis\\Intelisis5000\\Codigo Original\\'
-        + DrkBx.intls.newPath.maviToEsp(pathFile)
-    }
+const espFiltFls = R.pipe(
+    DrkBx.mix.fls.getFiltFls,
+    omitFls
+)
+
+const oppDir = R.pipe(
+    espFiltFls,
+    R.map(DrkBx.intls.fnCmp.cutByExstInOrig)
+)
+
+const gtMergOrgEsp = obj => {
+    return R.set(R.lensProp('exst', obj),
+        DrkBx.intls.fnCmp.mergOrgEsp(
+            R.prop('exst', obj),
+            DrkBx.intls.newPath.toOrigFls(R.prop('path', obj))
+        ),
+        obj
+    )
+   
 }
 
-const resultSet = R.pipe(
-    R.prop('esp'),
-    DrkBx.mix.fls.getTxt,
-    DrkBx.intls.take.cmpAll
+const procces = R.pipe(
+    oppDir,
+    R.map(gtMergOrgEsp)
 )
 
-const setResult = obj => R.set(R.lensProp('esp'), resultSet(obj), obj)
-
-const zipWithAddComp = R.curry(( comp, file) => R.zipWith(addComp, [comp], [file]))
-
-const zipPropsWithFn = obj => R.prop('esp', obj).forEach(
-    comp => zipWithAddComp(comp, R.prop('orig', obj))
-)
-
-
-const nameComp = R.pipe(
-    R.replace(/(?<=^\[).*?\//g, ''),
-    DrkBx.intls.take.cmpHead,
-    R.join('')
-)
-
-const rgxNameComp = R.pipe(nameComp, DrkBx.mix.adapt.toRegExp, toRgxNameComp)
-
-const addCompExist = R.curry( (comp, pathFile) => {
-    let headNewComp = nameComp(comp)
-    if(headNewComp) {
-        let newCompFields = DrkBx.intls.take.fldFull(comp)
-        if (newCompFields) {
-            newCompFields.forEach(fieldSend => {
-                let rgxAddField = new RegExp(`(?<=\\[\\b${DrkBx.mix.adapt.toRegExp(headNewComp)}\\b\\])`,`g`)
-                fs.appendFileSync('Reporte.txt','\n\nExist Comp: [' + headNewComp + ']\nNew Field In Exist Comps: \n' + fieldSend)
-                fs.writeFileSync(pathFile, DrkBx.mix.fls.getTxt(pathFile).replace(rgxAddField, '\n' + fieldSend), 'latin1')
-                return DrkBx.mix.fls.getTxt(pathFile).replace(rgxAddField, '\n' + fieldSend)
-            })
-        }
-    }
-});
-
-const addCompInexist = R.curry( (comp, pathFile) => {
-    fs.appendFileSync(pathFile, '\n[' + nameComp(comp) + ']\n'+ DrkBx.intls.take.fldFull(comp))
-    fs.appendFileSync('Reporte.txt', '\n[' + nameComp(comp) + ']\n'+ DrkBx.intls.take.fldFull(comp))
-    return '\n[' + nameComp(comp) + ']\n'+ DrkBx.intls.take.fldFull(comp)
-})
-
-const existComp = (comp, pathFile) => R.test( rgxNameComp(comp), DrkBx.mix.fls.getTxt(pathFile) )
-
-const addComp = (comp, pathFile) => (existComp(comp, pathFile)) ? addCompExist(comp)(pathFile) : addCompInexist(comp)(pathFile)
-
-const espflsOk = R.pipe(
-    DrkBx.mix.fls.getFiltFls,
-    omitFls,
-    R.map(origFls),
-    R.map(setResult),
-    R.forEach(zipPropsWithFn)
-)
-
-espflsOk('.esp', 'Data\\')
+// procces('.esp', 'Data\\')
+console.log(procces('.esp', 'Data\\'))
